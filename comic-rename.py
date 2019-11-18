a72b2c1b121237ad3166b8ae2fb018ff0c86b76d
JCarlee <jcarlee@gmail.com>
2019-11-18 14:06:50 -0500

    Simple working, needs testing

---
+++ comic-rename.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import filedialog
+import os
+import re
+
+root = tk.Tk()
+root.withdraw()
+
+# Select folder
+comic_folder = filedialog.askdirectory()
+
+comics = []
+for root, dirs, files in os.walk(comic_folder):
+    for file in files:
+        os.rename(os.path.join(root, file), os.path.join(root, re.sub(r'\([^.]*\.', '.', file)))
+
+
+# Isolate file type
+# a = 'asd(2015)sjnnf.cbz'
+# b = re.sub(r'\([^.]*\.', '.', a)
+
+# Isolate (YYYY)
+
+for comic in comics:
+    pass
