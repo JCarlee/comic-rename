1193112eef205e77261d491c191ace1630782995
John Carlee <jcarlee@gmail.com>
2019-12-03 13:42:37 -0500

    if name main

--- comic-rename.py
+++ comic-rename.py
@@ -3,18 +3,24 @@ from tkinter import filedialog
 import os
 import re
 
-root = tk.Tk()
-root.withdraw()
 
-# Select folder
-comic_folder = filedialog.askdirectory()
+def main():
+    a = tk.Tk()
+    a.withdraw()
 
-for root, dirs, files in os.walk(comic_folder):
-    for file in files:
-        x = re.search(r'\([0-9][0-9][0-9][0-9]\)', file)
-        try:
-            x.span()
-        except:
-            continue
-        xspan = x.span()
-        os.rename(os.path.join(root, file), os.path.join(root, file[:xspan[1]] + file[-4:]))
+    # Select folder
+    comic_folder = filedialog.askdirectory()
+
+    for root, dirs, files in os.walk(comic_folder):
+        for file in files:
+            x = re.search(r'\([0-9][0-9][0-9][0-9]\)', file)
+            try:
+                x.span()
+            except:
+                continue
+            xspan = x.span()
+            os.rename(os.path.join(root, file), os.path.join(root, file[:xspan[1]] + file[-4:]))
+
+
+if __name__ == '__main__':
+    main()
