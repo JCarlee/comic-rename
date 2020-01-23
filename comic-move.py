ec639e6df59a2dd4bf2a3ed201da8fa9c5625f8e
John Carlee <jcarlee@gmail.com>
2020-01-23 15:08:59 -0500

    delete duplicates

--- comic-move.py
+++ comic-move.py
@@ -18,10 +18,17 @@ list_ph = ["item1", "item2"]  # Example search terms to be edited
 
 # Prompt user to select starting folder
 comic_folder = filedialog.askdirectory()
+filenames = []
 
 # Main loop
 for root, dirs, files in os.walk(comic_folder):
     for file in files:
+        if file not in filenames:
+            filenames.append(file)
+        elif file in filenames:
+            gone = os.path.join(root, file)
+            os.remove(gone)
+            print("{0} deleted.".format(gone))
         if any(x in file.lower() for x in list_x):  # If any values in list_x are in filename, execute next line
             shutil.move(os.path.join(root, file), xmen)
         elif "one item" in file.lower():  # If "one item" in filename, execute next line
