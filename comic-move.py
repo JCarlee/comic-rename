435573f613fc118a1499ce753e9c8fc9291a93c1
John Carlee <jcarlee@gmail.com>
2020-01-24 10:40:59 -0500

    move if duped or multiple matches

--- comic-move.py
+++ comic-move.py
@@ -20,12 +20,22 @@ events = r"/Users/carlosbell/Library/Mobile Documents/com~apple~CloudDocs/Comics
 starwars = r"/Users/carlosbell/Library/Mobile Documents/com~apple~CloudDocs/Comics/_StarWars"
 reviewdelete = r"/Users/carlosbell/Library/Mobile Documents/com~apple~CloudDocs/Comics/__ReviewDelete"
 
+dupes = r"/Users/carlosbell/Library/Mobile Documents/com~apple~CloudDocs/ComicDupes"
+if not os.path.exists(dupes):
+    os.mkdir(dupes)
+
+multi = r"/Users/carlosbell/Library/Mobile Documents/com~apple~CloudDocs/ComicMultiMatch"
+if not os.path.exists(multi):
+    os.mkdir(multi)
+
+
 # --------- DEFINE LISTS ---------
 # Use only lower case values
 list_x = ["dead man logan", "x-men", "wolverine", "x-force", "new mutants", "fallen angels", "marauders", "excalibur",
-          "house of x", "powers of x"]  # Search terms to move into _Dawn of X
+          "house of x", "powers of x"]
 list_av = ["strikeforce", "jane foster", "doctor strange", "hawkeye", "thor", "star", "avenger", "hulk", "black widow",
-           "champions", "fantastic four", "captain america", "ms. marvel", "captain marvel", "iron man", "loki", "black panther", "ironheart", "invisible woman", "future foundation"]
+           "champions", "fantastic four", "captain america", "ms. marvel", "captain marvel", "iron man", "loki",
+           "black panther", "ironheart", "invisible woman", "future foundation"]
 list_sp = ["carnage", "venom", "spider", "black cat", "mary jane", "morbius"]
 list_co = ["galaxy", "annihilation", "nova", "silver surfer", "quill", "yondu", "thanos", "eternals"]
 list_othrm = ["punisher", "moon knight", "invaders", "daredevil", "conan", "runaways", "ghost rider", "doctor doom",
@@ -35,50 +45,89 @@ list_event = ["contagion", "absolute carnage", "annihilation", "ravencroft", "th
 list_starwars = ["star wars"]
 list_review = ["aero", "squirrel girl", "sword master", "tarot", "gwenpool", "marvel preview"]
 
+list_lists = [list_x, list_av, list_sp, list_co, list_othrm, list_event, list_starwars, list_review]
+
 # Prompt user to select starting folder
 comic_folder = filedialog.askdirectory()
 filenames = []
 
-# Main loop
-for root, dirs, files in os.walk(comic_folder):
-    for file in files:
-        if file not in filenames:
-            filenames.append(file)
-        elif file in filenames:
-            gone = os.path.join(root, file)
-            os.remove(gone)
-            print("{0} deleted.".format(gone))
-            break
-        if any(x in file.lower() for x in list_x):  # If any values in list_x are in filename, execute next line
-            m = os.path.join(root, file)
-            shutil.move(m, xmen)
-            print("{0} moved to {1}.".format(m, xmen))
-        elif any(x in file.lower() for x in list_ph):  # SAMPLE TEST USING LIST (list_ph)
-            shutil.move(os.path.join(root, file), placeholder)
-            print
+
+def comic_move(start, dest):
+    shutil.move(start, dest)
+    print("{0} moved to {1}.".format(start, dest))
+
 
 for root, dirs, files in os.walk(comic_folder):
     for file in files:
-        if file not in filenames:
-            filenames.append(file)
-        elif file in filenames:
-            gone = os.path.join(root, file)
-            os.remove(gone)
-            print("{0} deleted.".format(gone))
+        if file not in filenames:  # If filename unique
+            filenames.append(file)  # Add unique filename to filenames
+        elif file in filenames:  # If filename not unique
+            comic_move(os.path.join(root, file), dupes)  # Move to dupe folder
             break
         if any(x in file.lower() for x in list_x):  # If any values in list_x are in filename, execute next line
-            shutil.move(os.path.join(root, file), xmen)
+            dbl_chk = list_lists.pop(0)  # Create list of all other possible values
+            if any(y in file.lower() for y in dbl_chk):  # If match any other values
+                comic_move(os.path.join(root, file), multi)  # Move to multi folder
+            else:
+                comic_move(os.path.join(root, file), xmen)
         if any(x in file.lower() for x in list_av):
-            shutil.move(os.path.join(root, file), avengers)
+            dbl_chk = list_lists.pop(1)
+            if any(y in file.lower() for y in dbl_chk):
+                comic_move(os.path.join(root, file), multi)
+            else:
+                comic_move(os.path.join(root, file), avengers)
         if any(x in file.lower() for x in list_sp):
-            shutil.move(os.path.join(root, file), spiderman)
+            dbl_chk = list_lists.pop(2)
+            if any(y in file.lower() for y in dbl_chk):
+                comic_move(os.path.join(root, file), multi)
+            else:
+                comic_move(os.path.join(root, file), spiderman)
         if any(x in file.lower() for x in list_co):
-            shutil.move(os.path.join(root, file), cosmic)
+            dbl_chk = list_lists.pop(3)
+            if any(y in file.lower() for y in dbl_chk):
+                comic_move(os.path.join(root, file), multi)
+            else:
+                comic_move(os.path.join(root, file), cosmic)
         if any(x in file.lower() for x in list_othrm):
-            shutil.move(os.path.join(root, file), other_marvel)
+            dbl_chk = list_lists.pop(4)
+            if any(y in file.lower() for y in dbl_chk):
+                comic_move(os.path.join(root, file), multi)
+            else:
+                comic_move(os.path.join(root, file), other_marvel)
         if any(x in file.lower() for x in list_event):
-            shutil.move(os.path.join(root, file), events)
+            dbl_chk = list_lists.pop(5)
+            if any(y in file.lower() for y in dbl_chk):
+                comic_move(os.path.join(root, file), multi)
+            else:
+                comic_move(os.path.join(root, file), events)
         if any(x in file.lower() for x in list_starwars):
-            shutil.move(os.path.join(root, file), starwars)
+            dbl_chk = list_lists.pop(6)
+            if any(y in file.lower() for y in dbl_chk):
+                comic_move(os.path.join(root, file), multi)
+            else:
+                comic_move(os.path.join(root, file), starwars)
         if any(x in file.lower() for x in list_review):
-            shutil.move(os.path.join(root, file), reviewdelete)
+            dbl_chk = list_lists.pop(7)
+            if any(y in file.lower() for y in dbl_chk):
+                comic_move(os.path.join(root, file), multi)
+            else:
+                comic_move(os.path.join(root, file), reviewdelete)
+
+# Main loop
+# for root, dirs, files in os.walk(comic_folder):
+#     for file in files:
+#         if file not in filenames:
+#             filenames.append(file)
+#         elif file in filenames:
+#             gone = os.path.join(root, file)
+#             os.remove(gone)
+#             print("{0} deleted.".format(gone))
+#             break
+#         if any(x in file.lower() for x in list_x):  # If any values in list_x are in filename, execute next line
+#             m = os.path.join(root, file)
+#             shutil.move(m, xmen)
+#             print("{0} moved to {1}.".format(m, xmen))
+#         elif any(x in file.lower() for x in list_ph):  # SAMPLE TEST USING LIST (list_ph)
+#             shutil.move(os.path.join(root, file), placeholder)
+
+
